17083044dca4ececf06938f7dd5b77c3879991c6
lizwachira <lizwanguwachira@gmail.com>
2024-04-10 01:29:20 +0300

    send with fetch/catch error

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ http://localhost:3000/users
 http://localhost:3000/robots the live servers available
 */
 //http://localhost:3000/users, fetch function sends a POST request 
+//for users we have  no return value from thunder client ...
 function submitData(name,email){
     return fetch ('http://localhost:3000/users',{
-        method: "POST",
+        method: "POST", //fetch function sends a POST request
         headers:{
-            "Content-Type":"application/json",
+            "Content-Type":"application/json", 
             "Accept":"application/json"
         },
         body: JSON.stringify({
@@ -20,7 +21,7 @@ function submitData(name,email){
 .then(function(response){
     return response.json()
 })
-.then(function(object){
+.then(function(object){ //innerHTML property of the body element is either set/ or catches an error.
     document.body.innerHTML = object["id"]
 
 })
